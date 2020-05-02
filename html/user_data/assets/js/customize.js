b5d5d8d23e8da69b3dd9e9febcc1deaa61ab809c
jotasugiyama <c0117165b3@edu.teu.ac.jp>
2020-05-02 22:34:08 +0900

    [delete, fix] Delete ID and fix customize.js

--- html/user_data/assets/js/customize.js
+++ html/user_data/assets/js/customize.js
@@ -1,10 +1,11 @@
 /* カスタマイズ用Javascript */
-
-$("#js-nav").hover(function(){
-    $(this).children('.ec-itemNavBrand__children').slideDown(300);
-    $(this).children('.ec-itemNavBrand__anchor').css("background-color", "#f5f5f5");
-    $(this).children('.ec-itemNavBrand__children').css("display", "flex");
-}, function() {
-    $(this).children('.ec-itemNavBrand__children').slideUp('fast');
-    $(this).children('.ec-itemNavBrand__anchor').css("background-color", "#ffffff");
+$(function(){
+    $(".ec-itemNavBrand").hover(function(){
+        $(this).children('.ec-itemNavBrand__children').slideDown(300);
+        $(this).children('.ec-itemNavBrand__anchor').css("background-color", "#f5f5f5");
+        $(this).children('.ec-itemNavBrand__children').css("display", "flex");
+    }, function() {
+        $(this).children('.ec-itemNavBrand__children').slideUp('fast');
+        $(this).children('.ec-itemNavBrand__anchor').css("background-color", "#ffffff");
+    });
 });
