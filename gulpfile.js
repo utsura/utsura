f1e146c4037b1141802a547deb91ce3a6af826a0
jotasugiyama <c0117165b3@edu.teu.ac.jp>
2020-05-06 22:48:55 +0900

    [fix] gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,57 +1,27 @@
 const gulp = require("gulp");
-const $ = require("gulp-load-plugins")();
-const sass = require('gulp-sass');
-const rename = require('gulp-rename');
-const cleanCSS = require('gulp-clean-css');
-const es = require('event-stream');
 
-const srcPattern = [
-    'default',
-    'admin'
-];
+// 画像を圧縮するプラグインの読み込み
+const imagemin = require("gulp-imagemin");
+const mozjpeg = require("imagemin-mozjpeg");
+const pngquant = require("imagemin-pngquant");
+const changed = require("gulp-changed");
 
-// CSS出力
-gulp.task('sass', function() {
-    let events = srcPattern.map((target) => {
-        return gulp.src(`./html/template/${target}/assets/scss/**/*.scss`)
-            .pipe($.plumber({
-                errorHandler: $.notify.onError('<%= error.message %>')
-            }))
-            .pipe($.sourcemaps.init())
-            .pipe(sass({
-                sourceMap: true
-            }))
-            .pipe($.pleeease({
-                autoprefixer: true,
-                minifier: false,
-                mqpacker: true
-            }))
-            .pipe($.sourcemaps.write('./maps'))
-            .pipe(gulp.dest(`./html/template/${target}/assets/css/`))
-    });
-    return es.concat(events);
+// srcImgフォルダのjpg,png画像を圧縮して、distImgフォルダに保存する
+gulp.task("default", function() {
+  return gulp
+    .src(".top/*.{png,jpg, jpeg}") // srcImgフォルダ以下のpng,jpg画像を取得する
+    .pipe(changed("distImg")) // srcImg と distImg を比較して異なるものだけ圧縮する
+    .pipe(
+      imagemin([
+        pngquant({
+            quality: [ 0.65, 0.8 ], // 文字列から配列型に変更
+            speed: 1 // スピード
+        }),
+        mozjpeg({
+            quality: 85, // 画質
+            progressive: true
+        })
+      ])
+    )
+    .pipe(gulp.dest("./top/")); //保存
 });
-
-// CSS圧縮(cssディレクトリ直下のファイルだけ対象とする)
-gulp.task('minify-css', function() {
-    let events = srcPattern.map((target) => {
-        return gulp.src([`./html/template/${target}/assets/css/*.css`, `!./html/template/${target}/assets/css/**/*.min.css`])
-            .pipe($.plumber({
-                errorHandler: $.notify.onError('<%= error.message %>')
-            }))
-            .pipe($.sourcemaps.init())
-            .pipe(sass({
-                sourceMap: true
-            }))
-            .pipe(cleanCSS())
-            .pipe(rename({
-                extname: '.min.css'
-            }))
-            .pipe($.sourcemaps.write('./maps'))
-            .pipe(gulp.dest(`./html/template/${target}/assets/css/`))
-    });
-    return es.concat(events);
-});
-
-gulp.task("default", ["sass", "minify-css"]);
-
